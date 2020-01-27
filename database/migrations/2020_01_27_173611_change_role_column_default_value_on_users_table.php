1c6a892d614ae323bba8af7d348061ed66a255de
MrCrankHank <mail@alexander-hank.de>
2020-01-27 17:37:16 +0100

    Make readonly the default role for new users

---
+++ database/migrations/2020_01_27_173611_change_role_column_default_value_on_users_table.php
@@ -0,0 +1,20 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class ChangeRoleColumnDefaultValueOnUsersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->string('role')->default('readonly')->change();
+        });
+    }
+}
