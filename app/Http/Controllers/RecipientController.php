f810d3af5a5a31e4d74805b65254c6e17b8e1bad
Alexander Hank <mail@hank-it.com>
2022-02-19 00:14:54 +0100

    wip

--- app/Http/Controllers/RecipientController.php
+++ app/Http/Controllers/RecipientController.php
@@ -4,34 +4,35 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\RelayRecipient;
+use Illuminate\Validation\Rule;
 
 class RecipientController extends Controller
 {
     public function index(Request $request)
     {
         $this->validate($request, [
-            'search' => 'nullable|string',
-            'currentPage' => 'required|int',
-            'perPage' => 'required|int',
+            'search' => ['nullable', 'string'],
+            'currentPage' => ['required', 'int'],
+            'perPage' => ['required', 'int'],
         ]);
 
         $recipients = $request->filled('search')
-            ? RelayRecipient::where('payload', 'LIKE', '%' . $request->search . '%')
+            ? RelayRecipient::where('payload', 'LIKE', '%' . $request->input('search') . '%')
             : RelayRecipient::query();
 
         return response()->json([
             'status' => 'success',
             'message' => null,
-            'data' => $recipients->paginate($request->perPage, ['*'], 'page', $request->currentPage),
+            'data' => $recipients->paginate($request->input('perPage'), ['*'], 'page', $request->input('currentPage')),
         ]);
     }
 
     public function store(Request $request)
     {
         $this->validate($request, [
-           'payload' => 'required|string|unique:relay_recipients',
-           'action' => 'required|string|in:OK',
-           'data_source' => 'required|string|in:local',
+           'payload' => ['required', 'string', Rule::unique('relay_recipients')],
+           'action' => ['required', 'string', Rule::in(['OK'])],
+           'data_source' => ['required', 'string', Rule::in(['local'])],
         ]);
 
         return response()->json([
